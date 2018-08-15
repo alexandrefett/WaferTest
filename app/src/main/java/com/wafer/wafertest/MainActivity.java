9d80f15cb38dc3599f0413a1224a34aa1eb0ac67
Alexandre <alexandrefett@gmail.com>
2018-08-14 22:47:05 -0300

    commit

--- app/src/main/java/com/wafer/wafertest/MainActivity.java
+++ app/src/main/java/com/wafer/wafertest/MainActivity.java
@@ -1,21 +1,120 @@
 package com.wafer.wafertest;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+import com.wafer.wafertest.model.Country;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    public ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        try {
+            showDialog();
+            getCountry();
+        }
+        catch(JSONException e){
+            hideDialog();
+            Toast.makeText(getBaseContext(), R.string.error, Toast.LENGTH_LONG).show();
+        }
+    }
 
+    public void showDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+            mProgressDialog.setCancelable(false);
+        }
+        mProgressDialog.show();
+    }
+
+    public void hideDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    private void getCountry() throws JSONException{
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        Request jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                "https://restcountries.eu/rest/v2/all",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Process the JSON
+                        List<Country> countryArray = new ArrayList<Country>();
+                        try{
+                            // Loop through the array elements
+                            for(int i=0;i<response.length();i++){
+                                // Get current json object
+                                JSONObject country = response.getJSONObject(i);
+
+                                // Get the current country (json object) data
+                                String name = country.getString("name");
+
+                                JSONArray currencies = country.getJSONArray("currencies");
+                                String currency = currencies.getJSONObject(0).getString("name");
+
+                                JSONArray languages = country.getJSONArray("languages");
+                                String language = languages.getJSONObject(0).getString("name");
+
+                                // Add country to list
+                                countryArray.add(new Country(name, currency, language));
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                        hideDialog();
+                        mRecyclerView.setAdapter(null);
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        // Do something when error occurred
+                        hideDialog();
+                        Toast.makeText(getBaseContext(), R.string.error, Toast.LENGTH_LONG).show();
+                    }
+                }
+
+        );
+        requestQueue.add(jsonArrayRequest);
+
+    }
 }
--- app/src/main/java/com/wafer/wafertest/model/Country.java
+++ app/src/main/java/com/wafer/wafertest/model/Country.java
@@ -1,55 +1,31 @@
 package com.wafer.wafertest.model;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Country {
     String name;
-    List<String> languages;
-    List<String> currency;
+    String language;
+    String currency;
 
     public String getName() {
         return name;
     }
 
-    public List<String> getLanguages() {
-        return languages;
+    public String getLanguage() {
+        return language;
     }
 
-    public List<String> getCurrency() {
+    public String getCurrency() {
         return currency;
     }
 
-    public Country(String name, List<String> languages, List<String> currency) throws JSONException{
+    public Country(String name, String currency, String language){
         this.name = name;
-        this.languages = languages;
+        this.language = language;
         this.currency = currency;
     }
 }
 
     //{
     // "name":"Afghanistan",
-    // "topLevelDomain":[".af"],
-    // "alpha2Code":"AF",
-    // "alpha3Code":"AFG",
-    // "callingCodes":["93"],
-    // "capital":"Kabul",
-    // "altSpellings":["AF","Afġānistān"],
-    // "region":"Asia",
-    // "subregion":"Southern Asia",
-    // "population":27657145,
-    // "latlng":[33.0,65.0],
-    // "demonym":"Afghan",
-    // "area":652230.0,
-    // "gini":27.8,
-    // "timezones":["UTC+04:30"],
-    // "borders":["IRN","PAK","TKM","UZB","TJK","CHN"],
-    // "nativeName":"افغانستان",
-    // "numericCode":"004",
     // "currencies":[{
     //      "code":"AFN",
     //      "name":"Afghan afghani",
@@ -62,13 +38,3 @@ public class Country {
     // {"iso639_1":"uz","iso639_2":"uzb","name":"Uzbek","nativeName":"Oʻzbek"},
     // {"iso639_1":"tk","iso639_2":"tuk","name":"Turkmen","nativeName":"Türkmen"}
     // ],
-    // "translations":{
-    //      "de":"Afghanistan",
-    //      "es":"Afganistán",
-    //      "fr":"Afghanistan",
-    //      "ja":"アフガニスタン",
-    //      "it":"Afghanistan",
-    //      "br":"Afeganistão",
-    //      "pt":"Afeganistão","nl":"Afghanistan","hr":"Afganistan","fa":"افغانستان"},
-    // "flag":"https://restcountries.eu/data/afg.svg",
-    // "regionalBlocs":[{"acronym":"SAARC","name":"South Asian Association for Regional Cooperation","otherAcronyms":[],"otherNames":[]}],"cioc":"AFG"}
